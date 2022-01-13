1c3bc8d03c658cc8031053c479fde4c901f3130c
kalunge <muthomititus+github@gmail.com>
2022-01-13 15:42:19 +0100

    Add Api Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-
-  
-
+  namespace :api do
+    namespace :v1 do
+      resources :helicopters, only: [:index, :create, :destroy, :show]
+      resources :users, only: [:create] do
+        resources :reservations, only: [:index, :create, :destroy]
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
