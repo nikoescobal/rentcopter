504d176ff1222cd1613aa0ff4484d575201b020a
kalunge <muthomititus+github@gmail.com>
2022-01-17 15:21:09 +0100

    Update User Params

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.where(email: params[:email]).first
     if user&.valid_password?(params[:password])
-      render json: user.as_json(only: [:id, :email]), status: :created 
+      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created 
     else
       head(:unauthorized)
     end
