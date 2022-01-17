63969eb76e5ff7b86ab6ff107b7ab2b4ee7d42ac
Reshy <reshy@protonmail.com>
2022-01-17 15:32:44 +0100

    Add components

--- src/pages/Helicopters.js
+++ src/pages/Helicopters.js
@@ -1,8 +1,8 @@
 const Helicopters = () => (
-  <>
+  <div>
     <h1>Helicopters</h1>
     <p>You will be able to see the helicopters in this page.</p>
-  </>
+  </div>
 );
 
 export default Helicopters;
