f7d2ac7b304bbca299ef15e6eb4b6aae6a114e18
kalunge <muthomititus+github@gmail.com>
2022-01-17 15:13:43 +0100

    Add authentication token to users table

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.where(email: params[:email]).first
-    if user.valid_password?(params[:password])
+    if user&.valid_password?(params[:password])
       render json: user.as_json(only: [:id, :email]), status: :created 
     else
       head(:unauthorized)
