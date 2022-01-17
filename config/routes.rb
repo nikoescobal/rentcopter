75ffad3aea5c64bc5c41fef9d43644667f5d8612
kalunge <muthomititus+github@gmail.com>
2022-01-17 15:05:24 +0100

    IMplement create sessions to return email and Id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :users
   namespace :api do
     namespace :v1 do
       resources :helicopters, only: [:index, :create, :destroy, :show]
@@ -11,3 +10,4 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
