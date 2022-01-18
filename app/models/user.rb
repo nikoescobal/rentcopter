673d7f060501c8a9c5d3321e20a224e11bff785b
kalunge <muthomititus+github@gmail.com>
2022-01-18 15:47:19 +0100

    Resolve rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :helicopters, dependent: :destroy
   has_many :reservations, dependent: :destroy
 
-  validates :name, :email, :password,  presence: true
+  validates :name, :email, :password, presence: true
 end
