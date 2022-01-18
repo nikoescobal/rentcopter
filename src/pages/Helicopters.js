24213470e0435e65cd806f96c3271f7efb60f415
Reshy <reshy@protonmail.com>
2022-01-18 16:28:46 +0100

    Create new view for helicopters

--- src/pages/Helicopters.js
+++ src/pages/Helicopters.js
@@ -1,15 +1,16 @@
 import { useSelector } from 'react-redux';
+import HeliCard from '../components/HeliCard';
 
 const Helicopters = () => {
   const helicopters = useSelector((state) => state.helicopters.helicopters_arr);
 
   return (
-    <div>
-      <h1>Helicopters</h1>
-      <p>You will be able to see the helicopters in this page.</p>
-      <ul>
+    <div className="p-4 md:p-24 flex justify-center flex-col w-full h-screen">
+      <h1 className="cool-title text-center !text-5xl">Helicopters</h1>
+      <p className="text-center">You will be able to see the helicopters in this page.</p>
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-evenly mt-20 m-auto w-full h-full" style={{ gap: '20px' }}>
         {helicopters.map((helicopter) => (
-          <li key={helicopter}>{helicopter}</li>
+          <HeliCard key={helicopter.id} helicopter={helicopter} />
         ))}
       </ul>
     </div>
