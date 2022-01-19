e6663167db779a88acd74ee109ba2c6c04ed9b70
Niko Escobal <niko.escobal@gmail.com>
2022-01-19 17:27:05 +0800

    Add migrations to add capacity, flying range and flying speed to Helicopters table.

--- app/controllers/api/v1/helicopters_controller.rb
+++ app/controllers/api/v1/helicopters_controller.rb
@@ -49,6 +49,6 @@ class Api::V1::HelicoptersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def helicopter_params
-    params.permit(:name, :model, :image, :description, :rental_cost)
+    params.permit(:name, :model, :image, :description, :rental_cost, :capacity, :flying_range, :flying_speed)
   end
 end
