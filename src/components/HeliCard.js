c8505d59ce41e295c12538a1248e9d23b57a12fc
Reshy <reshy@protonmail.com>
2022-01-19 16:45:44 +0100

    Add remove heli page

--- src/components/HeliCard.js
+++ src/components/HeliCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { CardActionArea } from '@mui/material';
-import Money from './Money';
+import { useLocation } from 'react-router-dom';
+import LowerCardDelete from './LowerCardDelete';
+import LowerCardReserve from './LowerCardReserve';
 
 /* eslint-disable */
 
-const HeliCard = (props) => (
-    <div className='w-full sm:w-2/3 md:w-full mx-auto md:h-60 rounded-2xl flex' style={{
+const HeliCard = (props) => {
+    const location = useLocation();
+
+    return(
+    <div className='w-full sm:w-2/3 md:w-full mx-auto md:h-60 rounded-2xl flex hover:shadow-2xl hover:shadow-gray-600 transition-all' style={{
         backgroundImage: `url(${props.helicopter.img})`, 
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
@@ -22,33 +27,13 @@ const HeliCard = (props) => (
             <div className="h-10 w-full bg-gray-900 bg-opacity-75 rounded-t-md">
                 <h1 className="cool-title ml-4 my-auto text-left h-full">{props.helicopter.name}</h1>
             </div>
-            
+        
         </CardActionArea>
-        <div className=" bg-gray-900 w-full h-1/4 flex flex-row-reverse justify-between bg-opacity-50">
-                <button className="
-                h-2/3
-                sm:h-1/2 
-                my-auto 
-                rounded-full 
-                mr-5
-                text-sm
-                bg-opacity-75
-                w-24
-                 font-bold 
-                bg-yellow-600
-                hover:bg-indigo-700
-                transition-all
-                text-white">RESERVE</button>
-                <div className="ml-3 my-auto flex">
-                    <Money />
-                    <p className="ml-2 text-xs text-white " style={{fontFamily: 'Montserrat'}}>Price per day:<br/><span className="text-md font-bold">{props.helicopter.price_per_day}$</span></p>
-                </div>
-                
-            </div>
+        {location.pathname == '/remove-helicopter/' ? <LowerCardDelete helicopter={props.helicopter} /> : <LowerCardReserve helicopter={props.helicopter} />}
             
     </div>
     
     
-);
+);}
 
 export default HeliCard;
--- src/pages/RemoveHelicopter.js
+++ src/pages/RemoveHelicopter.js
@@ -1,11 +1,27 @@
-const RemoveHelicopter = () => (
-  <div>
-    <h1>Remove Helicopter</h1>
-    <p>
-      You will have a form displayed in this page. You will be able to remove a
-      new helicpter using that form.
-    </p>
-  </div>
-);
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import HeliCard from '../components/HeliCard';
 
-export default RemoveHelicopter;
+const RemoveHelicpoter = () => {
+  const helicopters = useSelector((state) => state.helicopters.helicopters_arr);
+  useEffect(() => {
+    const parent = document.getElementById('main-heli');
+    setTimeout(() => {
+      parent.classList.replace('opacity-0', 'opacity-1');
+      parent.classList.replace('scale-0', 'scale-100');
+    }, 150);
+  }, []);
+  return (
+    <div id="main-heli" className="p-4 md:p-24 flex justify-center flex-col w-full lg:w-5/6 h-screen transition-all opacity-0 scale-0">
+      <h1 className="cool-title text-center !text-5xl">Delete a helicopter</h1>
+      <p className="text-center">You will be able to see the helicopters in this page.</p>
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-evenly mt-20 m-auto w-full h-full" style={{ gap: '20px' }}>
+        {helicopters.map((helicopter) => (
+          <HeliCard key={helicopter.id} helicopter={helicopter} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RemoveHelicpoter;
