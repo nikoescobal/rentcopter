1d7420ad7ef6756924e77c9bb92a37679de64b79
Reshy <reshy@protonmail.com>
2022-01-20 18:56:08 +0100

    Add fonts and styling

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import {
 } from 'react-router-dom';
 import Helicopters from './pages/Helicopters';
 import Reservations from './pages/Reservations';
-import Reserve from './pages/Reserve';
+import Homepage from './pages/Homepage';
 import AddHelicopter from './pages/AddHelicopter';
 import RemoveHelicopter from './pages/RemoveHelicopter';
 import ErrorPage from './pages/ErrorPage';
 import Navbar from './components/Navbar';
 import LogIn from './pages/LogIn';
+import Register from './pages/Register';
 
 const App = () => {
   const { pathname } = window.location;
   return (
     <>
       <Router>
-        {pathname !== '/login' && pathname !== 'register' ? <Navbar /> : null }
+        {pathname !== '/login' && pathname !== '/register' && pathname !== '/login/' && pathname !== '/register/' ? <Navbar /> : null }
         <Routes>
           <Route path="/login" element={<LogIn />} />
-          <Route path="/" element={<Helicopters />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/reservations" element={<Reservations />} />
-          <Route path="/add-reservation" element={<Reserve />} />
+          <Route path="/add-reservation" element={<Helicopters />} />
           <Route path="/add-helicopter" element={<AddHelicopter />} />
           <Route path="/remove-helicopter" element={<RemoveHelicopter />} />
           <Route path="*" element={<ErrorPage />} />
