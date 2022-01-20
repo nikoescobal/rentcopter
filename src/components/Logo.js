618c48e1733857636c2aeeffae301d699c4b5d58
Reshy <reshy@protonmail.com>
2022-01-20 18:53:58 +0100

    Add logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { downTop } from '../animations';
 
-const Logo = () => (
-  <div className="
+const Logo = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="
     absolute
     top-0
     w-full
-    border-b"
-  >
-    <NavLink className="text-center w-full" to="/">
-      <img
-        src="https://i.imgur.com/MKWxT1E.png"
-        alt="logo"
-        className="
+    border-b
+    shadow-2xl
+    shadow-black"
+    >
+      <NavLink
+        className="text-center w-full"
+        to="/"
+        onClick={(e) => {
+          e.preventDefault();
+          if (window.location.pathname !== '/') {
+            downTop();
+            setTimeout(() => {
+              navigate('/');
+            }, 250);
+          }
+        }}
+      >
+        <img
+          src="https://i.imgur.com/MKWxT1E.png"
+          alt="logo"
+          className="
             w-3/4
             m-auto"
-      />
-    </NavLink>
-  </div>
-);
+        />
+      </NavLink>
+    </div>
+  );
+};
 
 export default Logo;
