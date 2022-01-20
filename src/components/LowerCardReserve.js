aecf1d2bb5ef7e4e0f69ce366737879d8a4efb56
Reshy <reshy@protonmail.com>
2022-01-20 18:54:52 +0100

    Add modals

--- src/components/LowerCardReserve.js
+++ src/components/LowerCardReserve.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import Money from './Money';
+import HeliReserve from './HeliReserve';
 /* eslint-disable */
 
 const LowerCardReserve = (props) => (
   <div className=" bg-gray-900 w-full h-1/4 flex flex-row-reverse justify-between bg-opacity-50">
-    <button className="
-                h-2/3
-                sm:h-1/2
-                my-auto
-                rounded-full
-                mr-5
-                text-sm
-                bg-opacity-75
-                w-24
-                 font-bold
-                bg-yellow-600
-                hover:bg-indigo-700
-                transition-all
-                text-white"
-    >
-      RESERVE
-    </button>
+    <HeliReserve />
     <div className="ml-3 my-auto flex">
       <Money />
       <p className="ml-2 text-xs text-white " style={{ fontFamily: 'Montserrat' }}>
