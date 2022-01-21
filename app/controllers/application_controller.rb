05a0fd261fb3d243bdca7b8a0cebbe016ea2a175
Reshy <reshy@protonmail.com>
2022-01-21 13:56:05 +0100

    Fix registration issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::API
         ]
       }, status: :bad_request
     end
-  end
+
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    end
+end
