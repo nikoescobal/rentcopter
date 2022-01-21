d820d62f227fd94f25366ea0856e501c7705e7d3
Reshy <reshy@protonmail.com>
2022-01-21 18:02:37 +0100

    Expose Authorization header in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '*', headers: %w(Authorization),
+    methods: :any,
+    expose: %w(Authorization),
+    max_age: 600
   end
 end
