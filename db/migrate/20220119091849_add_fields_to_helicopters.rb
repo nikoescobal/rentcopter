1a19f45ab623caa5fe0098e200d4b0b54d79bc5c
Reshy <reshy@protonmail.com>
2022-01-24 18:26:31 +0100

    Add image field to migrations

--- db/migrate/20220119091849_add_fields_to_helicopters.rb
+++ db/migrate/20220119091849_add_fields_to_helicopters.rb
@@ -3,5 +3,6 @@ class AddFieldsToHelicopters < ActiveRecord::Migration[6.1]
     add_column :helicopters, :capacity, :integer
     add_column :helicopters, :flying_range, :integer
     add_column :helicopters, :flying_speed, :integer
+    add_column :helicopters, :image, :string
   end
 end
