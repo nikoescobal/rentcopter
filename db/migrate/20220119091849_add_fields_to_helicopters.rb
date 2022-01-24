8ccabca49113a9f35c6d4923fe8959dbafdb342f
Reshy <reshy@protonmail.com>
2022-01-24 18:38:57 +0100

    Add new images to schema

--- db/migrate/20220119091849_add_fields_to_helicopters.rb
+++ db/migrate/20220119091849_add_fields_to_helicopters.rb
@@ -3,6 +3,5 @@ class AddFieldsToHelicopters < ActiveRecord::Migration[6.1]
     add_column :helicopters, :capacity, :integer
     add_column :helicopters, :flying_range, :integer
     add_column :helicopters, :flying_speed, :integer
-    add_column :helicopters, :image, :string
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ user2 = User.create!(name:"Niko", username:"Niko", email:"niko.escobal@gmail.com
 user3 = User.create!(name:"Reshy", username:"Reshy", email:"reshyresh@gmail.com", password:"123456", admin: true)
 user4 = User.create!(name:"Ruben", username:"Ruben", email:"rpire@gmail.com", password:"123456", admin: true)
 
-copter1 = Helicopter.create!(name: "Copter-X", model:"Z", image:"copter-x.png", description: "Ultra Luxury Heli. Comes with jacuzzi.", rental_cost:50000, capacity:6, flying_range:350, flying_speed:260, user_id:1)
-copter2 = Helicopter.create!(name: "Apache-Y", model:"Z", image:"apache-y.png", description: "Military Escort Heli. Comes with three soldiers.",rental_cost:40000,  capacity:8, flying_range:450, flying_speed:460, user_id:2)
-copter3 = Helicopter.create!(name: "Deli-Heli", model:"Z", image:"deli-heli.png", description: "Get your deli meats with Deli-heli. Comes with charcuterie and gelato.",rental_cost:40000,  capacity:10, flying_range:360, flying_speed:250, user_id:3)
-copter4 = Helicopter.create!(name: "Airstriker", model:"Z", image:"airstriker.png", description: "Airstrikes on demand. Comes with a desired hitman of choice to finish off remaining targets.",rental_cost:100000, capacity:12, flying_range:750, flying_speed:560,  user_id:4)
+copter1 = Helicopter.create!(name: "Copter-X", model:"Z", image:"https://luxcior.com/wp-content/uploads/2021/03/Beverly-Hills-Magazine-luxury-helicopters-for-sale-private-helicopter-EUROCOPTERS-5.jpg", description: "Ultra Luxury Heli. Comes with jacuzzi.", rental_cost:50000, capacity:6, flying_range:350, flying_speed:260, user_id:1)
+copter2 = Helicopter.create!(name: "Apache-Y", model:"Z", image:"https://www.wheremilan.com/wp-content/uploads/2017/09/helitaly-1.jpg", description: "Military Escort Heli. Comes with three soldiers.",rental_cost:40000,  capacity:8, flying_range:450, flying_speed:460, user_id:2)
+copter3 = Helicopter.create!(name: "Deli-Heli", model:"Z", image:"https://www.beverlyhillsmagazine.com/wp-content/uploads/Beverly-Hills-Magazine-shop-luxury-helicopters-for-sale-private-helicopter-AGUSTA-3.jpg", description: "Get your deli meats with Deli-heli. Comes with charcuterie and gelato.",rental_cost:40000,  capacity:10, flying_range:360, flying_speed:250, user_id:3)
+copter4 = Helicopter.create!(name: "Airstriker", model:"Z", image:"https://www.xluxuo.com/wp-content/uploads/2020/10/Hill-Helicopters-HX50-1-scaled-1.jpg", description: "Airstrikes on demand. Comes with a desired hitman of choice to finish off remaining targets.",rental_cost:100000, capacity:12, flying_range:750, flying_speed:560,  user_id:4)
 
 reservation1 = Reservation.create!(date_start:Time.now, date_end:Time.now, user_id:1, helicopter_id:1)
 reservation2 = Reservation.create!(date_start:Time.now, date_end:Time.now, user_id:2, helicopter_id:2)
