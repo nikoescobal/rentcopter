4a41987517cdb3b1ec0bc1a0f44d2d43d337c63a
Reshy <reshy@protonmail.com>
2022-01-24 11:26:49 +0100

    Change reservations params

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ReservationsController < ApplicationController
-  before_action :set_reservation, only: %i[show update destroy]
 
   # GET /reservations
   def index
@@ -15,19 +14,9 @@ class Api::V1::ReservationsController < ApplicationController
   # POST /reservations
   def create
     @reservation = current_user.reservations.new(reservation_params)
-    @reservation.helicopter_id = params[:helicopter_id]
 
     if @reservation.save
       render json: @reservation, status: :created
-    else
-      render json: @reservation.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /reservations/1
-  def update
-    if @reservation.update(reservation_params)
-      render json: @reservation
     else
       render json: @reservation.errors, status: :unprocessable_entity
     end
@@ -40,13 +29,8 @@ class Api::V1::ReservationsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_reservation
-    @reservation = Reservation.find(params[:id])
-  end
-
   # Only allow a list of trusted parameters through.
   def reservation_params
-    params.require(:reservation).permit(:date_start, :date_end)
+    params.require(:reservation).permit(:date_start, :date_end, :helicopter_id)
   end
 end
